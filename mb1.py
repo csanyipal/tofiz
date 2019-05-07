ba4f4326e90348988b4d949e3e6a1c0c6472468a
Csányi Pál <csanyipal@gmail.com>
2019-05-07 09:16:24 +0200

    Add files via upload
    Added codes for the start of mb1.

--- mb1.py
+++ mb1.py
@@ -4,35 +4,45 @@
 from microbit import *
 # import radio
 
+# A rádió címzésének, csoportjának, és erejének beállítása.
+# Podešavanje adrese, grupe i jačine za radio.
+# radio.config(address="tofiz")
+# radio.config(group=0)
+# radio.config(power=7)
+
 # Reszet, vagy a microbit bekapcsolása után.
 # Posle uključivanja ili resetovanja mikrobita.
 
 while True:
     if button_a.was_pressed():
         # Ha az A gomb le lett nyomva legalább egyszer,
-        # akkor mutassa az "S" jelet.
+        # akkor mutassa a "Go!" szöveget,
+        # majd az "A" betűt, s végül a "<-" képet.
         # Ezután indulhat a versenyző!
         # Ako je dugme A pritisnuto bar jedanput,
-        # treba prikazati "S" karakter.
+        # treba prikazati "Go!" tekst,
+        # pa "A" slovo i najzad "<-" sliku.
         # Takmičar može krenuti!
-        display.show("S")
+        display.scroll("Go!")
+        display.show("A")
+        sleep(500)
+        display.show(Image.ARROW_W)
+        sleep(500)
         break
     else:
-        # Különben mutassa az "MB1" jelet.
-        # Inače treba prikazati neprekidno "MB1" tekst.
+        # Különben mutassa az "MB1" szöveget,
+        # majd az "A" betűt, s végül a "<-" képet.
+        # Inače treba prikazati neprekidno "MB1" tekst,
+        # pa "A" slovo i najzad "<-" sliku.
         display.scroll("MB1")
+        display.show("A")
+        sleep(500)
+        display.show(Image.ARROW_W)
+        sleep(500)
 
-# A bírónak figyelnie kell, hogy az A gomb lenyomása után
-# észrevegye az S betűt.
-# Sudija treba da obrati svoju pažnju na mikrobit
-# da bi zapazio S karakter.
-sleep(2000)
-
-# Miután a bíró észrevette az S betűt, jöhet a következő feladata.
 # Ezek után a főbíró - akinél az mb1 van - elszámol háromtól egyig így:
 # három, kettő, egy; és e pillanatban megnyomja az mb1 A gombját.
-# Kad je sudija zapazio znak S, sledi njegov sledeći zadatak.
-# Sudija - kod kojeg je mb1 - treba da odbroji unazad od tri do jedan ovako:
+# Sada sudija - kod kojeg je mb1 - treba da odbroji od tri do jedan ovako:
 # tri, dva, jedan; i u tom trenutku treba da pritisne dugme A na mb1.
 while True:
     if button_a.was_pressed():
@@ -50,3 +60,6 @@ while True:
         # Jelzi, hogy le kell nyomni az A gombot.
         # Prikazuje A što znači da treba pritisnuti dugme A.
         display.show("A")
+        sleep(500)
+        display.show(Image.ARROW_W)
+        sleep(500)
