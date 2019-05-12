664acd1e75f685f190fa984f239b1f6761581f0b
Csányi Pál <csanyipal@gmail.com>
2019-05-12 09:12:34 +0200

    Add files via upload
    Almost completed the code.

--- mb1.py
+++ mb1.py
@@ -9,58 +9,138 @@ import radio
 radio.config(group=0)
 radio.config(power=7)
 
+# Megjelenítendő képek:
+# Slike za pokazivanje:
+# Znak "1".
+kep1 = Image("09000:"
+             "99000:"
+             "09000:"
+             "09000:"
+             "09000")
+
+# Znak "A".
+kepA = Image("09900:"
+             "90090:"
+             "99990:"
+             "90090:"
+             "90090:")
+
+# Znak "B".
+kepB = Image("09990:"
+             "09009:"
+             "09990:"
+             "09009:"
+             "09990:")
+
+# Znak za pešačenje.
+kepFutas = Image("00900:"
+                 "99999:"
+                 "00900:"
+                 "09090:"
+                 "90009:")
+
+# Znak "->".
+kepK = Image("00900:"
+             "00090:"
+             "55559:"
+             "00090:"
+             "00900:")
+
+# Znak "<-".
+kepNY = Image("00900:"
+              "09000:"
+              "95550:"
+              "09000:"
+              "00900:")
+
+# Znak "?!".
+kepKerdFelkialt = Image("99009:"
+                        "00909:"
+                        "09009:"
+                        "00000:"
+                        "09009")
+
 # Reszet, vagy a microbit bekapcsolása után.
 # Posle uključivanja ili resetovanja mikrobita.
 
 while True:
     if button_a.was_pressed():
         # Ha az A gomb le lett nyomva legalább egyszer,
-        # akkor mutassa a "Go!" szöveget,
-        # majd az "A" betűt, s végül a "<-" képet.
-        # Ezután indulhat a versenyző!
+        # akkor kiléphetünk e körből.
         # Ako je dugme A pritisnuto bar jedanput,
-        # treba prikazati "Go!" tekst,
-        # pa "A" slovo i najzad "<-" sliku.
-        # Takmičar može krenuti!
+        # onda se može izaći iz ovog ciklusa.
         break
     else:
-        # Különben mutassa az "MB1" szöveget,
-        # majd az "A" betűt, s végül a "<-" képet.
-        # Inače treba prikazati neprekidno "MB1" tekst,
-        # pa "A" slovo i najzad "<-" sliku.
-        display.scroll("MB1")
-        display.show("A")
+        # Különben mutassa az "1" szöveget,
+        # majd "<-" képet, s végül az "A" betűt.
+        # Inače treba prikazati neprekidno "1" tekst,
+        # pa "<-" sliku i najzad "A" slovo.
+        display.show(kep1)
         sleep(500)
-        display.show(Image.ARROW_W)
+        display.show(kepNY)
+        sleep(500)
+        display.show(kepA)
         sleep(500)
 
 # Ezek után a főbíró - akinél az mb1 van - elszámol háromtól egyig így:
-# három, kettő, egy; és e pillanatban megnyomja az mb1 A gombját.
+# három, kettő, egy; és e pillanatban megnyomja az mb1 B gombját.
 # Sada sudija - kod kojeg je mb1 - treba da odbroji od tri do jedan ovako:
-# tri, dva, jedan; i u tom trenutku treba da pritisne dugme A na mb1.
+# tri, dva, jedan; i u tom trenutku treba da pritisne dugme B na mb1.
 while True:
-    if button_a.was_pressed():
-        # Jelzi, hogy a rádiójel el lett küldve mb2 -nek, és
-        # el is küldi a jelet, ami egy kis s betű,
-        # miután az A gomb le lett nyomva az mb1 -en.
-        # Prikazuje da je radio signal ( malo slovo s )
-        # poslat mb2 mikrobitu posle pritiska dugmeta A na mb1.
-        # Takođe šalje taj signal.
-        display.show("R")
+    if button_b.was_pressed():
+        # Elküldi az IdoIndul szöveget,
+        # miután a B gomb le lett nyomva az mb1 -en.
+        # Šalje "IdoIndul" signal mb2 mikrobitu posle
+        # pritiska dugmeta B na mb1.
+        # IdoIndul je MerenjeVremenaPocinje
         radio.on()
-        radio.send("started")
+        radio.send("IdoIndul")
+        sleep(100)
         radio.off()
         break
     else:
-        # Jelzi, hogy le kell nyomni az A gombot.
-        # Prikazuje A što znači da treba pritisnuti dugme A.
-        # Takmičar može krenuti!
-        display.scroll("Go!")
-        display.show("A")
-        sleep(500)
-        display.show(Image.ARROW_W)
-        sleep(500)
+        # Jelzi, hogy le kell nyomni a B gombot.
+        # Prikazuje B što znači da treba pritisnuti dugme B.
+        display.show(kepB)
 
-# Várnia kell, hogy mb2 elküldhesse a lépések számát.
-# 5 másodpercet vár.
-sleep(5000)
+# Most már a versenyző elindult,
+# az "IdoIndul" rádióüzenet el lett küldve mb2 -nek.
+# Sad je takmičar već krenuo, radio signal je već poslato na mb2.
+display.show("R")
+
+radio.on()
+
+while True:
+    # fogottJel je dobijeniSignal
+    fogottJel = radio.receive()
+    if fogottJel is not None:
+        # Ha megjött a jel, akkor mutassa a futás képét.
+        # Ako je stigao signal onda treba prikazati sliku trčanja.
+        break
+    else:
+        # Különben mutassa a "?!" jelet.
+        # Inače treba prikazati "?!" znak.
+        display.show(kepKerdFelkialt)
+
+# Megkétszerezzük a lépések számát.
+# Udvostručava se broj koraka.
+lepesek = int(fogottJel) * 2
+
+# Most ki kell számolnunk a sebességet.
+# A megtett út egyenlő a lépések megkétszerezett számának,
+# és a lépés átlaghosszának szorzatával.
+# Sada se izračunava brzina.
+# Put je jednako sa proizvodom udvostručenih koraka
+# i prosečne dužine koraka.
+# atlagLepesHossz je prosecnaDuzinaKoraka
+# ut je put
+# lepesek je koraci
+# sebesseg je brzina
+atlagLepesHossz = 0.66355
+ut = lepesek * atlagLepesHossz
+sebesseg = ut / 10
+
+while True:
+    # Megjelenítjük a sebességet m/sec -ben.
+    # Prikazuje se brzina u m/sec.
+    display.scroll(str(sebesseg))
--- mb2.py
+++ mb2.py
@@ -1,52 +1,123 @@
-# mb1 programozása
-# programiranje mb1
+# mb2 programozása
+# programiranje mb2
 
 from microbit import *
 import radio
+import utime
 
 # A rádió címzésének, csoportjának, és erejének beállítása.
 # Podešavanje adrese, grupe i jačine za radio.
 radio.config(group=0)
 radio.config(power=7)
 
-lepesek = 0
-ido = 0
-azElkuldottRadioJelMegerkezett = 0
+# Megjelenítendő képek:
+# Slike za pokazivanje:
+# Znak "?!".
+kepKerdFelkialt = Image("99009:"
+                        "00909:"
+                        "09009:"
+                        "00000:"
+                        "09009")
+
+# Znak "2".
+kep2 = Image("09000:"
+             "90900:"
+             "00900:"
+             "09000:"
+             "99900")
+
+# Znak "A".
+kepA = Image("09900:"
+             "90090:"
+             "99990:"
+             "90090:"
+             "90090:")
+
+# Znak "B".
+kepB = Image("09990:"
+             "09009:"
+             "09990:"
+             "09009:"
+             "09990:")
+
+# Znak za pešačenje.
+kepFutas = Image("00900:"
+                 "99999:"
+                 "00900:"
+                 "09090:"
+                 "90009:")
+
+# Znak "->".
+kepK = Image("00900:"
+             "00090:"
+             "55559:"
+             "00090:"
+             "00900:")
+
+# Znak "<-".
+kepNY = Image("00900:"
+              "09000:"
+              "95550:"
+              "09000:"
+              "00900:")
+
+# korak
+lepes = 0
 
 # Reszet, vagy a microbit bekapcsolása után
 # Posle uključivanja ili resetovanja mikrobita.
 
+while True:
+    if button_a.was_pressed():
+        break
+    else:
+        # Különben mutassa a "2" szöveget,
+        # majd az "<-" nyilat, s végül a "A" képet.
+        # Inače treba prikazati neprekidno "2" tekst,
+        # pa "<-" i najzad "A".
+        display.show(kep2)
+        sleep(500)
+        display.show(kepNY)
+        sleep(500)
+        display.show(kepA)
+        sleep(500)
+
 radio.on()
 
 while True:
+    # fogottJel je dobijeniSignal
     fogottJel = radio.receive()
-    if fogottJel == "started":
-        versenyzoElindult = 1
-        display.show(Image.RABBIT)
+    if fogottJel is not None:
+        # Ha megjött a jel, akkor mutassa a gyaloglás képét.
+        # Ako je stigao signal onda treba prikazati sliku pešačenja.
+        display.show(kepFutas)
+        break
     else:
-        versenyzoElindult = 0
-        # Különben mutassa az "MB2" szöveget.
-        # Inače treba prikazati neprekidno "MB2" tekst.
-        display.scroll("MB2")
+        # Különben a "?!" jelet mutassa.
+        # Inače se prikazuje "?!" znak.
+        display.show(kepKerdFelkialt)
 
+radio.off()
+
+# Elkezdi mérni az időt tíz másodpercig.
+# Počinje meriti vreme do dest sekundi.
+# mostan je sada
+mostan = int(utime.ticks_ms() / 1000)
 while True:
-    if versenyzoElindult == 1:
-        radio.off()
-        ido += 1
-        if ido < 10:
-            if accelerometer.was_gesture("down"):
-                lepesek += 1
+    if int(utime.ticks_ms() / 1000) > mostan + 10:
+        break
+    else:
+        if accelerometer.was_gesture("3g"):
+            # povećava se broj koraka
+            lepes += 1
 
-lepesek = lepesek * 2
+display.show(Image.HAPPY)
+sleep(1000)
 
-# Ismételgeti a rádióadást, míg nem kap jelet, hogy mb1 vette a jelet.
-while True:
-    if azElkuldottRadioJelMegerkezett == 0:
-        radio.on()
-        radio.send(str(lepesek))
-        fogottJel = radio.receive()
-        if fogottJel == "megjott":
-            azElkuldottRadioJelMegerkezett = 1
-
-
+# Megvannak a lépések, amit mb2 összeszámolt tíz mp alatt.
+# mb2 -nek el kell küldenie mb1 számára e lépések számát!
+# Sada imamo broj koraka učinjenih za deset sekundi.
+# Sada treba mb2 to poslati za mb1!
+radio.on()
+radio.send(str(lepes))
 radio.off()
